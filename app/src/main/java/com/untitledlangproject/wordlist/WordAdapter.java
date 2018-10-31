7a8caf78dc4a147c38b3d404233cb64626a6326c
Fernando Batista <fernandohsbatista@gmail.com>
2018-10-31 15:00:00 -0200

    The click on the "x" button are handled, and the button that had a "check" are no longer available

--- app/src/main/java/com/untitledlangproject/wordlist/WordAdapter.java
+++ app/src/main/java/com/untitledlangproject/wordlist/WordAdapter.java
@@ -2,9 +2,11 @@ package com.untitledlangproject.wordlist;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -32,14 +34,22 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.MyViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
 
-        WordItem word = new WordItem(
+        final WordItem word = new WordItem(
                 words.get(position).getText(),
                 words.get(position).getNumberOfUsage());
 
         holder.tvWord.setText(word.getText());
         holder.tvNumberOfUsages.setText(String.valueOf(word.getNumberOfUsage()));
+
+        holder.imgNoKeep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                words.remove(position);
+                notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -50,14 +60,15 @@ public class WordAdapter extends RecyclerView.Adapter<WordAdapter.MyViewHolder> 
     class MyViewHolder extends RecyclerView.ViewHolder{
 
         private TextView tvWord, tvNumberOfUsages;
-        private ImageView imgNoKeep, imgKeep;
+        private Button imgNoKeep;
 
         MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tvWord = itemView.findViewById(R.id.tv_word);
             tvNumberOfUsages = itemView.findViewById(R.id.tv_number_of_usages);
             imgNoKeep = itemView.findViewById(R.id.img_no_keep);
-            imgKeep = itemView.findViewById(R.id.img_keep);
         }
+
+
     }
 }
